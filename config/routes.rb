18b54b8503002a7773a672aa3a078834f435f8ef
brian corpas <briancorpas@gmail.com>
2020-10-19 16:31:29 +0100

    home page sorted

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+  get 'about', to: 'pages#about'
+  get 'contacts', to: 'pages#contact'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :products, only: [:index, :show]
+
+
+  get "profile", to: "pages#profile", as: "profile"
+
 end
