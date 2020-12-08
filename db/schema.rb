c256e3dd022d364bc805aa04bb1c017fa7294eaa
brian corpas <briancorpas@gmail.com>
2020-12-08 16:27:51 +0000

    rails admin implemented

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_04_191323) do
+ActiveRecord::Schema.define(version: 2020_12_08_161553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 2020_12_04_191323) do
     t.string "last_name"
     t.string "phone_number"
     t.string "address"
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
