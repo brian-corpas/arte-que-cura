e64878d8f4cda21810829e1634fc17d68f1d54d4
brian corpas <briancorpas@gmail.com>
2021-02-26 13:47:34 +0000

    orders controller refactored

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,26 +1,51 @@
 class OrdersController < ApplicationController
-  def create
-    product = Product.find(params[:product_id])
-    order = Order.create!(product: product, name: product.name, amount: product.price, state: 'pending', user: current_user)
-
-      session = Stripe::Checkout::Session.create(
-        payment_method_types: ['card'],
-        line_items: [{
-          name: product.name,
-          images: [product.photo.key],
-          amount: product.price_cents,
-          currency: 'eur',
-          quantity: 1
-        }],
-        success_url: order_url(order),
-        cancel_url: order_url(order)
-      )
-
-      order.update(checkout_session_id: session.id)
-      redirect_to new_order_payment_path(order)
+  def index
+    @orders = current_user.orders
   end
 
   def show
     @order = current_user.orders.find(params[:id])
+  end
+
+  def new
+    @order = Order.new
+  end
+
+  def create
+    cart = Cart.find(params[:cart_id])
+    order = Order.create!(state: 'pending', user: current_user)
+    order.line_items = cart.line_items
+    line_items = []
+    order.line_items.each do |item|
+      line_items.push({
+        name: item.product.name,
+        images: [item.product.photo.key],
+        amount: item.product.price_cents,
+        currency: 'eur',
+        quantity: item.quantity
+        })
+    end
+
+    session = Stripe::Checkout::Session.create(
+      payment_method_types: ['card'],
+      mode: 'payment',
+      line_items: line_items,
+      success_url: order_url(order),
+      cancel_url: order_url(order)
+    )
+
+    order.update(checkout_session_id: session.id)
+    redirect_to new_order_payment_path(order)
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:name, :email, :address, :pay_method)
+
+  end
+
+  def line_items
+
   end
 end
